afd17bcd8e02c2c66941a5be1885a9d684a4e016
Abhijith <Abhijithkenchain@gmail.com>
2023-12-01 16:26:38 +0530

    asa

---
+++ GitHubPracticeAsATeam/src/test/java/practiceGit/ClassB.java
@@ -0,0 +1,14 @@
+package practiceGit;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassB {
+	
+	@Test
+	public void yum()
+	{
+		Reporter.log("Abhijith code",true);
+	}
+
+}
